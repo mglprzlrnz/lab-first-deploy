4d9f16aa20c0110e1d0269202ac2305e4986f8f1
Tuyet-Trinh thi Vu <mglprzlrnz@gmail.com>
2017-07-11 19:23:57 +0200

    nodemon added

--- routes/siteController.js
+++ routes/siteController.js
@@ -2,7 +2,7 @@ const express = require("express");
 const siteController = express.Router();
 
 siteController.get("/", (req, res, next) => {
-  res.render("site/index", { apiKey: "AIzaSyCTxsFM2Bbm4KEtuFAmdu11WF3s3zUliwU" });
+  res.render("site/index", { apiKey: "AIzaSyBrX9IUy98OnKdkhsl6c_Ds68FgG2igcpA" });
 });
 
 module.exports = siteController;
